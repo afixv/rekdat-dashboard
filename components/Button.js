d244631a85909c218e3646362bd9dcd536018ab0
afixv <afixv00@gmail.com>
2023-11-28 10:07:25 +0700

    fix: button styling

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ label, isActive, onClick }) => {
         isActive
           ? "bg-blue-500 hover:bg-blue-600 text-white"
           : "bg-gray-300 hover:bg-gray-400 text-gray-700"
-      } rounded focus:outline-none focus:ring focus:border-blue-300 transition-all duration-300`}
+      } rounded focus:outline-none focus:border-blue-300 transition-all duration-300`}
       onClick={() => onClick()}>
       {label}
     </button>
